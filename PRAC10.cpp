3cdeaa5bb2be4790e2af8e7b7e3a4f56847efaf2
immukultiwari <immukultiwari@gmail.com>
2025-08-02 23:25:17 +0530

    Add files via upload

---
+++ PRAC10.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+void arraymaker(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    return ;
+}
+int mincheck(int arr[],int n){
+    int SMALL=INT_MAX;
+    for(int i=0;i<n;i++){
+        if(arr[i]<SMALL){
+            SMALL=arr[i];
+        }
+    }
+    return SMALL;
+}
+int main(){
+    int n;
+    cout<<"enter the number of elements you want";
+    cin>>n;
+    int arr[n];
+    cout<<"enter the elements you want";
+    arraymaker(arr,n);
+    cout<<"smallest number is"<<mincheck(arr,n);
+    return 0;
+
+}
