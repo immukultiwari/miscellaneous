958e61f79edbb3beb20a3693c16dd639b504431b
immukultiwari <immukultiwari@gmail.com>
2025-08-04 13:06:32 +0530

    Add files via upload

---
+++ PRAC11.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+void arraymaker(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    return ;
+
+}
+int keysearch(int arr[],int n,int key){
+    for(int i=0;i<n;i++){
+        if(arr[i]==key){
+            return i;
+        }
+    }
+    return -1;
+    }
+
+int main(){
+    int n;
+    cout<<"input number of elements you want to enter";
+    cin>>n;
+    int arr[n];
+    cout<<"enter the elements you wnt to enter";
+    arraymaker(arr,n);
+    int key;
+    cout<<"enter the element you want to search for";
+    cin>>key;
+    int x=keysearch(arr,n,key);
+    if(x==-1){
+        cout<<"not available";
+
+    }else{
+        cout<<"required index is"<<x;
+    }
+
+
+}
+
